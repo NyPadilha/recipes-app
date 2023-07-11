edec2db0bd6d1012bb23ffefdd77e6db99a04d99
alessandro98nascimento <alessandro-s-nascimento@hotmail.com>
2023-07-11 15:34:57 -0300

    feat: finalizando implementação das informações dos inputs ao estado

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,17 +1,67 @@
+import { useState } from 'react';
+
 function SearchBar() {
+  const [checkedInputs, setCheckedInputs] = useState(
+    {
+      ingredient: true,
+      name: false,
+      firstLetter: false,
+    },
+  );
+
+  const handleChange = ({ target }) => {
+    if (target.value === 'ingredient') {
+      const newState = { ingredient: true,
+        name: false,
+        firstLetter: false };
+      setCheckedInputs(newState);
+    } else if (target.value === 'name') {
+      const newState = { ingredient: false,
+        name: true,
+        firstLetter: false };
+      setCheckedInputs(newState);
+    } else {
+      const newState = { ingredient: false,
+        name: false,
+        firstLetter: true };
+      setCheckedInputs(newState);
+    }
+  };
+
   return (
     <>
       <label>
         Ingredient
-        <input type="radio" data-testid="ingredient-search-radio" />
+        <input
+          type="radio"
+          name="searchInput"
+          value="ingredient"
+          data-testid="ingredient-search-radio"
+          onChange={ handleChange }
+          checked={ checkedInputs.ingredient }
+        />
       </label>
       <label>
         Name
-        <input type="radio" data-testid="name-search-radio" />
+        <input
+          type="radio"
+          name="searchInput"
+          value="name"
+          data-testid="name-search-radio"
+          onChange={ handleChange }
+          checked={ checkedInputs.name }
+        />
       </label>
       <label>
         First Letter
-        <input type="radio" data-testid="first-letter-search-radio" />
+        <input
+          type="radio"
+          name="searchInput"
+          value="firstLetter"
+          data-testid="first-letter-search-radio"
+          onChange={ handleChange }
+          checked={ checkedInputs.firstLetter }
+        />
       </label>
       <button data-testid="exec-search-btn">SEARCH</button>
     </>
