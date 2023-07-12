3606e63b64e5b269280c7530aa55562c48a12cd2
Pedro Gabriel <pglfreitas@gmail.com>
2023-07-12 14:50:23 -0300

    Feat: push paras as paginas corretas, Test: 100% coverage do Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,21 +1,28 @@
+import { useHistory } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import './Footer.css';
 
 function Footer() {
+  const history = useHistory();
+
   return (
     <div
       data-testid="footer"
       className="footer"
     >
-      <button>
+      <button
+        onClick={ () => history.push('/drinks') }
+      >
         <img
           data-testid="drinks-bottom-btn"
           src={ drinkIcon }
           alt="drinkIcon"
         />
       </button>
-      <button>
+      <button
+        onClick={ () => history.push('/meals') }
+      >
         <img
           data-testid="meals-bottom-btn"
           src={ mealIcon }
