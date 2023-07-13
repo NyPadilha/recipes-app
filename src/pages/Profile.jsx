be2109c6e2d974a4853cd31b7b1033fa100986bd
Pedro Gabriel <pglfreitas@gmail.com>
2023-07-13 11:23:10 -0300

    Feat: push to done recepies

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 
 function Profile() {
   const [profileEmail, setProfileEmail] = useState('');
   const routeName = 'Profile';
+  const history = useHistory();
 
   useEffect(() => {
     const localEmail = JSON.parse(localStorage.getItem('user'));
@@ -16,7 +18,12 @@ function Profile() {
       <Header routeName={ routeName } />
       <div>
         <div data-testid="profile-email">{ profileEmail.email }</div>
-        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button
+          data-testid="profile-done-btn"
+          onClick={ () => { history.push('/done-recipes'); } }
+        >
+          Done Recipes
+        </button>
         <button data-testid="profile-favorite-btn">Favorite Recipes</button>
         <button data-testid="profile-logout-btn">Logout</button>
       </div>
