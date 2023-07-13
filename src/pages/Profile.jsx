0a9b49991acf420b54b1d824b1ff53a7e924bf85
Pedro Gabriel <pglfreitas@gmail.com>
2023-07-13 11:16:58 -0300

    Feat: Basic profile page, Req: 57,58,59

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,28 @@
+import { useState, useEffect } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 
-function Drinks() {
+function Profile() {
+  const [profileEmail, setProfileEmail] = useState('');
   const routeName = 'Profile';
+
+  useEffect(() => {
+    const localEmail = JSON.parse(localStorage.getItem('user'));
+    setProfileEmail(localEmail);
+  }, []);
+
   return (
     <>
       <Header routeName={ routeName } />
+      <div>
+        <div data-testid="profile-email">{ profileEmail.email }</div>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
       <Footer />
     </>
   );
 }
 
-export default Drinks;
+export default Profile;
